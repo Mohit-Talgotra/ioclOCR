d8f1372f9e8862ea13d92f0ef8fffdd31fe458b9
Mohit Talgotra <talgotram@gmail.com>
2025-07-07 16:56:47 +0530

    feat: added updated add-in code, and fixed port error

--- app/app.py
+++ app/app.py
@@ -323,5 +323,5 @@ if __name__ == '__main__':
     cleanup_thread = threading.Thread(target=periodic_cleanup, daemon=True)
     cleanup_thread.start()
     debug = os.environ.get('FLASK_ENV') == 'development'
-    
-    app.run(host='0.0.0.0', debug=debug)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', debug=debug, port=port)
